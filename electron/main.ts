08e3cc74b3f9b16d6627f000817c1b118e555760
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-09-29 17:39:36 +0200

    Refactor electron main.ts file, alter event names

--- electron/main.ts
+++ electron/main.ts
@@ -1,91 +1,103 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
-import path from 'node:path'
-import * as fs from "fs";
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'node:path';
+import * as fs from 'fs';
 
-// The built directory structure
-//
-// ├─┬─┬ dist
-// │ │ └── index.html
-// │ │
-// │ ├─┬ dist-electron
-// │ │ ├── main.js
-// │ │ └── preload.js
-// │
-process.env.DIST = path.join(__dirname, '../dist')
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+const DIST = path.join(__dirname, '../dist');
+const VITE_PUBLIC = app.isPackaged ? DIST : path.join(DIST, '../public');
 
-
-let win: BrowserWindow | null
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+let win: BrowserWindow | null;
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-  })
+  });
 
-  // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    win?.webContents.send('main-process-message', (new Date).toLocaleString());
+  });
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    win.loadURL(VITE_DEV_SERVER_URL);
   } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    win.loadFile(path.join(DIST, 'index.html'));
   }
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
-})
+// IPC Listeners for File Operations
+ipcMain.on('saveChatLog', (event, data, saveFilePath) => {
+  fs.writeFile(saveFilePath, JSON.stringify(data), err => {
+    if (err) {
+      console.error('An error occurred writing the file:', err);
+      event.sender.send('error', 'An error occurred saving the chat log.');
+    } else {
+      console.log('Data saved');
+    }
+  });
+});
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-ipcMain.on("saveChatLog", (sender, data, saveFilePath) =>{
-  console.log(data)
-  const sData = JSON.stringify(data)
-  fs.writeFileSync(saveFilePath, sData)
-  console.log("Data saved")
-})
-
-ipcMain.on("loadFile", (event, loadFilePath) => {
-  //const filePath = path.join(__dirname, "../data.json"); // replace with your file path
+ipcMain.on('loadConfigFile', (event, loadFilePath) => {
   fs.readFile(loadFilePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error("An error occurred reading the file:", err);
-      return;
+      console.error('An error occurred reading the file:', err);
+      event.sender.send('error', 'An error occurred loading the config file.');
+    } else {
+      event.sender.send('configData', data);
     }
-    event.sender.send("fileData", data);
   });
 });
 
-ipcMain.on("loadFilenames", (event, loadDirectoryPath) => {
-  //const directoryPath = path.join(__dirname, "../"); // replace with your directory path
+ipcMain.on('saveWorkingFile', (event, data, saveFilePath) => {
+  fs.writeFile(saveFilePath, (data), err => {
+    if (err) {
+      console.error('An error occurred writing the file:', err);
+      event.sender.send('error', 'An error occurred saving the working file.');
+    } else {
+      event.sender.send('saveWorkingFileSuccess', { message: 'File saved successfully' });
+    }
+  });
+});
+
+ipcMain.on('loadWorkingFile', (event, loadFilePath) => {
+  console.log(loadFilePath)
+
+  fs.readFile(loadFilePath, 'utf-8', (err, data) => {
+
+    if (err) {
+      console.error('An error occurred reading the file:', err);
+      event.sender.send('error', 'An error occurred loading the working file.');
+    } else {
+      event.sender.send('workingFile', data);
+    }
+  });
+});
+
+ipcMain.on('loadFilenames', (event, loadDirectoryPath) => {
   fs.readdir(loadDirectoryPath, (err, filenames) => {
     if (err) {
-      console.error("An error occurred reading the directory:", err);
-      return;
+      console.error('An error occurred reading the directory:', err);
+      event.sender.send('error', 'An error occurred loading the filenames.');
+    } else {
+      event.sender.send('filenamesData', filenames);
     }
-    console.log(filenames)
-    event.sender.send("filenamesData", filenames);
   });
 });
 
-app.whenReady().then(createWindow)
+// Application Lifecycle
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+    win = null;
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.whenReady().then(createWindow);
