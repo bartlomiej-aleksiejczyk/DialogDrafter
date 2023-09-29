747fa937aa27fe9183f969398ae481bfe98791d2
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-09-29 17:34:46 +0200

    Alter display setting of top navbar now it is fixed

--- src/components/topNavbar/TopNavbar.tsx
+++ src/components/topNavbar/TopNavbar.tsx
@@ -1,7 +1,6 @@
 export function TopNavbar() {
     return (
-        <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed bg-base-300 z-50">
                 <div className="flex-1">
                     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
                 </div>
@@ -9,12 +8,11 @@ export function TopNavbar() {
                     <button className="btn btn-square btn-ghost">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                              className="inline-block w-5 h-5 stroke-current">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
                         </svg>
                     </button>
                 </div>
             </div>
-        </div>
     );
 }
