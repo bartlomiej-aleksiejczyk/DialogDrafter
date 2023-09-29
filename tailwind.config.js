604718961043019c72b399e1091b517777054c57
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-09-29 17:31:59 +0200

    Add config for huge spacing, for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '104': '26rem',
+        '112': '28rem',
+        '120': '30rem',
+        '128': '32rem',
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
