cb9a79c768b6da7566809146cc0c6f8c5073606b
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-01 11:16:26 +0200

    Add info about platform to initial data

---
+++ src/components/initialConfig/LoadInitialData.tsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from "react";
+import {SetInitialData} from "./SetInitialData";
+import {defaultConfig} from "../../config/defaultConfig";
+import {getPathUntilLastSlash} from "./getPathUntilLastSlash";
+
+export function LoadInitialData() {
+
+    const [applicationConfig, setApplicationConfig] = useState<Record<string, any>>();
+    const [workingDirectory, setWorkingDirectory] = useState<string>();
+    const [userPlatform, setUserPlatform] = useState<string>();
+
+    useEffect(() => {
+        window.ipcRenderer.send('get-platform');
+        window.ipcRenderer.on('send-platform', (event, platform) => {
+            setUserPlatform(platform)
+        });
+        window.ipcRenderer.send("loadConfigFile", defaultConfig.SETTINGS_PATH);
+        const handleFileData =  (_event, data) => {
+            const parsedData = JSON.parse(data);
+            setApplicationConfig(parsedData);
+            setWorkingDirectory(getPathUntilLastSlash(parsedData["workingFile"]));
+
+        };
+        window.ipcRenderer.on("configData", handleFileData);
+        // TODO: clean up removeListeners
+        return () => {
+            window.ipcRenderer.removeListener("configData", handleFileData);
+            window.ipcRenderer.removeAllListeners('send-platform');
+        };
+    }, []);
+
+    return (
+        <>
+            {(applicationConfig && workingDirectory) && <SetInitialData
+                applicationConfig={applicationConfig}
+                setApplicationConfig={setApplicationConfig}
+                workingDirectory={workingDirectory}
+                setWorkingDirectory={setWorkingDirectory}
+                userPlatform={userPlatform as string}
+            />}
+        </>
+    )
+}
