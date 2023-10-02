c95402c0ca435951c0473884d245550dd6d11b32
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-02 12:53:59 +0200

    Add toast with success message

---
+++ src/shared/toasts/SuccessToast.tsx
@@ -0,0 +1,22 @@
+import {toast} from "react-hot-toast";
+
+export const SuccessToast = (t) => (
+    <div
+        className={`${
+            t.visible ? 'animate-enter' : 'animate-leave'
+        } max-w-md w-full pointer-events-auto alert alert-success`}
+    >
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="m7 10 2 2 4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+        </svg>
+        <div>
+            <h3 className="font-bold">Success!</h3>
+            <div className="text-xs">{t.message}</div>
+        </div>
+        <button onClick={() => toast.remove(t.id)} className="btn btn-sm">
+            Close
+        </button>
+    </div>
+)
