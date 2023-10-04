643c02953f0b7dfaf743c812e2e1119884ed56df
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-04 11:39:11 +0200

    Improve menus for SideMenu

--- src/components/sideMenu/SideMenu.tsx
+++ src/components/sideMenu/SideMenu.tsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import {SideMenuItem} from "./SideMenuItem";
 import {ApplicationConfigContext} from "../initialConfig/ApplicationConfigContext";
 import {outputOnlyMdFiles} from "./outputOnlyMdFiles";
@@ -6,33 +6,28 @@ import {ContextMenu, ContextMenuBody} from "./contextMenu/ContextMenuBody";
 import {handleRightClick} from "./contextMenu/handleRightClick";
 import {handleRemove} from "./contextMenu/handleRemove";
 import {handleRename} from "./contextMenu/handleRename";
+import {DropdownPortal} from "./dropdownPortal/DropdownPortal";
+import {handleButtonClick} from "./dropdownPortal/handleButtonClick";
 
 
 function SideMenu() {
-
-    // TODO: Check if transition App -> LoadInitialData -> SetInitialData -> [TopNavbar, SideMenu, ChatLogBoxContainer] is good idea
-
-    //TODO: Handle if directory path is wrong
-    // TODO: Handle if file is not set
-    // TODO: Check if working directory is available
-    // TODO: Check if working directory is in directory list
-    // TODO: Check if working file is available
-    // TODO: Check if working file is in working directory
-    // TODO: Error handling for I/O errors
-
-    // TODO: Add remove/rename file feature
     // TODO: Add remove/rename directory feature
 
-    // TODO: Examine accessing object key as strong e.g., "object["key"]"
-
     const [directoryContent, setDirectoryContent] = useState<string[]>([]);
+    const [visibleDropdown, setVisibleDropdown] = useState<string | null>(null);
+
+    const [isRemoveModalVisible, setIsRemoveModalVisible] = useState(false);
+    const [isRenameModalVisible, setIsRenameModalVisible] = useState(false);
+
     const [contextMenu, setContextMenu] = useState<ContextMenu>({
         x: 0,
         y: 0,
         isVisible: false,
         directoryKey: ''
     });
-
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const dropdownRef = useRef(null);
+    const buttonRef = useRef(null);
 
     const {applicationConfig, workingDirectory, setWorkingDirectory} = useContext(ApplicationConfigContext)
     const availableDirectories = applicationConfig["directories"]
@@ -41,10 +36,12 @@ function SideMenu() {
     };
 
     useEffect(() => {
-        const handleGlobalClick = () => setContextMenu(prev => ({...prev, isVisible: false}));
+        const handleGlobalClick = (e) => {
+        };
         window.addEventListener('click', handleGlobalClick);
         return () => window.removeEventListener('click', handleGlobalClick);
-    }, []);
+    }, [visibleDropdown]);
+
     useEffect(() => {
         window.ipcRenderer.send("loadFilenames", workingDirectory);
         window.ipcRenderer.once("filenamesData", handleFileData);
@@ -54,29 +51,50 @@ function SideMenu() {
     const handleSelectDirectory = (directoryPath) => {
         setWorkingDirectory(directoryPath)
     };
+
     return (
         <div className="inline fixed top-0 left-0 h-screen bg-base-200 text-base-content pt-16 overflow-hidden ">
-            <ul className="menu p-4 w-80 overflow-y-auto max-h-[calc(100vh-4rem)] flex-nowrap ">
+            <ul className={`menu p-4 w-80  max-h-[calc(100vh-4rem)] flex-nowrap ${visibleDropdown ? "overflow-hidden" : "overflow-y-auto" }`}>
                 {Object.entries(availableDirectories).map(([key, value]) => {
                     return (
-                        <li key={key} className="p-2"
-                            onClick={() => handleSelectDirectory(value)}
-                        >
-                            {(workingDirectory === value) ?
-                                <>
-                                    <a className="active w-60"
-                                       onContextMenu={(e) => handleRightClick(e, key, setContextMenu)}
-                                    >{key}</a>
-                                    <ul className={`menu-dropdown menu-dropdown-show`}>
-                                        {directoryContent.map(item => (
-                                            <SideMenuItem key={item} fileName={item} directory={value as string}/>
-                                        ))}
+                        <div key={key} className="flex">
+                            <li key={key} className="pt-2 pb-2  flex w-60 "
+                                onClick={() => handleSelectDirectory(value)}
+                            >
+                                {(workingDirectory === value) ?
+                                    <>
+                                        <a className="active w-60 inline"
+                                           onContextMenu={(e) => handleRightClick(e, key, setContextMenu)}
+                                        >{key}</a>
+                                        <ul className={`menu-dropdown menu-dropdown-show`}>
+                                            {directoryContent.map(item => (
+                                                <SideMenuItem key={item} fileName={item} directory={value as string}/>
+                                            ))}
+                                        </ul>
+                                    </>
+                                    : <a className="w-60">{key}</a>
+                                }
+                            </li>
+                            <button onClick={(e) => handleButtonClick(e, key, setPosition, setVisibleDropdown)}
+                                    ref={buttonRef}
+                                    className={`btn btn-sm btn-square ml-2 mt-2.5 ${visibleDropdown===key&&"btn-active"}`}>
+                                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true"
+                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
+                                    <path
+                                        d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+                                </svg>
+                            </button>
+                            {visibleDropdown === key && (
+                                <DropdownPortal position={position}>
+                                    <ul tabIndex={0}
+                                        className="dropdown-content mt-1 p-2 shadow menu shadow bg-base-100 rounded-box w-52"
+                                        ref={dropdownRef}>
+                                        <li><a>Rename item</a></li>
+                                        <li><a>Remove item</a></li>
                                     </ul>
-                                </>
-                                : <a className="w-60">{key}</a>
-                            }
-                        </li>
-
+                                </DropdownPortal>
+                            )}
+                        </div>
                     );
                 })}
                 <ContextMenuBody context contextMenu={contextMenu} handleRemove={handleRemove}
