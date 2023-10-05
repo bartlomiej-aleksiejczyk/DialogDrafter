3fca11d10834b3de4c5311b40f3af579d8310179
Bartłomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-05 22:59:47 +0200

    Finish modal

--- src/components/sideMenu/modals/RenameDirectoryModal.tsx
+++ src/components/sideMenu/modals/RenameDirectoryModal.tsx
@@ -13,7 +13,7 @@ type RenameDirectoryModalProps = {
 type RenameInput = {
     newName: string;
 };
-export const RenameDirectoryModal = ({directoryToRemove, isVisible, setIsVisible }: RenameDirectoryModalProps) => {
+export const RenameDirectoryModal = ({directoryToRemove, isVisible, setIsVisible}: RenameDirectoryModalProps) => {
     const {applicationConfig, setApplicationConfig} = useContext(ApplicationConfigContext)
     const {
         register,
@@ -23,58 +23,54 @@ export const RenameDirectoryModal = ({directoryToRemove, isVisible, setIsVisible
     const directories = applicationConfig["directories"]
     const isDirectoryNameAvailable = (directoryName: string) => !Object.keys(directories).includes(directoryName);
 
-    const handleConfirm = () => {
-        setIsVisible(false);
-        handl(directoryToRemove, applicationConfig, setApplicationConfig);
-    };
-
     const handleClose = () => {
         setIsVisible(false);
     };
     const onSubmit: SubmitHandler<RenameInput> = (data) => {
         console.log(data)
-        const newDirectories = {...directories, [data.directoryName]: getValues("selectedDirectory")}
-        setApplicationConfig({
-            ...applicationConfig,
-            "directories": newDirectories
-        })
-        document.getElementById('addDirectoryModal')?.close()
+        handleDirectoryRemove(data.newName, applicationConfig, setApplicationConfig)
+        setIsVisible(false)
     };
     return (
-        <>
+        <>`
             {isVisible && (
                 <dialog className="modal modal-bottom sm:modal-middle" open>
                     <div className="modal-box">
-                        <h3 className="font-bold text-lg">Confirmation</h3>
-                        <p className="py-4">{"MESSAGE_TEXT_TODO"}</p>
-                        <input
-                            {...register("directoryName", {
-                                required: errorMessages.directoryName.required,
-                                pattern: {
-                                    value: validFilenameRegex,
-                                    message: errorMessages.directoryName.pattern
-                                },
-                                maxLength: {
-                                    value: maxFilenameLength,
-                                    message: errorMessages.directoryName.maxLength
-                                },
-                                validate: {
-                                    isNameAvailable: value =>
-                                        isDirectoryNameAvailable(value) || errorMessages.directoryName.isNameAvailable
-                                }
-                            })}
-                            className="input input-bordered w-full max-w-xs"
-                            placeholder="Directory name"
-                        />
-                        {errors.newName && (
-                            <label className="label">
-                        <div className="modal-action">
-                            <button className="btn" onClick={handleConfirm}>Confirm</button>
-                            <button className="btn" onClick={handleClose}>Close</button>
-                        </div>
-                    </div>
-                </dialog>
-            )}
-        </>
-    );
-};
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <h3 className="font-bold text-lg">Confirmation</h3>
+                            <p className="py-4">{"MESSAGE_TEXT_TODO"}</p>
+                            <input
+                                {...register("directoryName", {
+                                    required: errorMessages.directoryName.required,
+                                    pattern: {
+                                        value: validFilenameRegex,
+                                        message: errorMessages.directoryName.pattern
+                                    },
+                                    maxLength: {
+                                        value: maxFilenameLength,
+                                        message: errorMessages.directoryName.maxLength
+                                    },
+                                    validate: {
+                                        isNameAvailable: value =>
+                                            isDirectoryNameAvailable(value) || errorMessages.directoryName.isNameAvailable
+                                    }
+                                })}
+                                className="input input-bordered w-full max-w-xs"
+                                placeholder="Directory name"
+                            />
+                            {errors.newName && (
+                                <label className="label">
+                                    <div className="modal-action">
+                                        <button className="btn" type="submit">Confirm</button>
+                                        <button className="btn" onClick={handleClose}>Close</button>
+                                    </div>
+                                </form>
+                                </div>
+                                </dialog>
+                                )}
+                        </>
+                        );
+                        };
+
+                        )
+                        }
