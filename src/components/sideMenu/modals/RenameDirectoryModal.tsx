911ac3c26b23e4100121f6a6468c1e37a333216a
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-07 03:28:51 +0200

    Refactor and repeair Rename modal

--- src/components/sideMenu/modals/RenameDirectoryModal.tsx
+++ src/components/sideMenu/modals/RenameDirectoryModal.tsx
@@ -1,19 +1,19 @@
 import React, {useContext} from 'react';
-import {handleDirectoryRemove} from "./handleDirectoryRemove";
 import {ApplicationConfigContext} from "../../initialConfig/ApplicationConfigContext";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {errorMessages} from "../../addDirectoryModal/errorMessages";
 import {maxFilenameLength, validFilenameRegex} from "../../../shared/validators/validators";
+import {GenericModalProps} from "../../../shared/interfaces/GenericModalProps";
+import {handleDirectoryRename} from "./handleDirectoryRename";
 
-type RenameDirectoryModalProps = {
-    directoryToRemove: string
-    isVisible: boolean;
-    setIsVisible: (isVisible: boolean) => void;
-};
+interface RenameDirectoryModalProps extends GenericModalProps {
+    directoryToRename: string
+}
+
 type RenameInput = {
     newName: string;
 };
-export const RenameDirectoryModal = ({directoryToRemove, isVisible, setIsVisible}: RenameDirectoryModalProps) => {
+export const RenameDirectoryModal = ({directoryToRename, isVisible, setIsVisible}: RenameDirectoryModalProps) => {
     const {applicationConfig, setApplicationConfig} = useContext(ApplicationConfigContext)
     const {
         register,
@@ -28,49 +28,45 @@ export const RenameDirectoryModal = ({directoryToRemove, isVisible, setIsVisible
     };
     const onSubmit: SubmitHandler<RenameInput> = (data) => {
         console.log(data)
-        handleDirectoryRemove(data.newName, applicationConfig, setApplicationConfig)
+        handleDirectoryRename(directoryToRename, data.newName, applicationConfig, setApplicationConfig)
         setIsVisible(false)
     };
     return (
-        <>`
-            {isVisible && (
-                <dialog className="modal modal-bottom sm:modal-middle" open>
-                    <div className="modal-box">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <h3 className="font-bold text-lg">Confirmation</h3>
-                            <p className="py-4">{"MESSAGE_TEXT_TODO"}</p>
-                            <input
-                                {...register("directoryName", {
-                                    required: errorMessages.directoryName.required,
-                                    pattern: {
-                                        value: validFilenameRegex,
-                                        message: errorMessages.directoryName.pattern
-                                    },
-                                    maxLength: {
-                                        value: maxFilenameLength,
-                                        message: errorMessages.directoryName.maxLength
-                                    },
-                                    validate: {
-                                        isNameAvailable: value =>
-                                            isDirectoryNameAvailable(value) || errorMessages.directoryName.isNameAvailable
-                                    }
-                                })}
-                                className="input input-bordered w-full max-w-xs"
-                                placeholder="Directory name"
-                            />
-                            {errors.newName && (
-                                <label className="label">
-                                    <div className="modal-action">
-                                        <button className="btn" type="submit">Confirm</button>
-                                        <button className="btn" onClick={handleClose}>Close</button>
-                                    </div>
-                                </form>
-                                </div>
-                                </dialog>
-                                )}
-                        </>
-                        );
-                        };
-
-                        )
-                        }
+        isVisible &&
+        <>
+            <dialog className="modal modal-bottom sm:modal-middle" open>
+                <div className="modal-box">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h3 className="font-bold text-lg">Confirmation</h3>
+                        <p className="py-4">{"MESSAGE_TEXT_TODO"}</p>
+                        <input
+                            {...register("newName", {
+                                required: errorMessages.directoryName.required,
+                                pattern: {
+                                    value: validFilenameRegex,
+                                    message: errorMessages.directoryName.pattern
+                                },
+                                maxLength: {
+                                    value: maxFilenameLength,
+                                    message: errorMessages.directoryName.maxLength
+                                },
+                                validate: {
+                                    isNameAvailable: value =>
+                                        isDirectoryNameAvailable(value) || errorMessages.directoryName.isNameAvailable
+                                }
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                            placeholder={directoryToRename}
+                        />
+                        {errors.newName &&
+                            <label className="label"> {errors.newName.message} </label>}
+                        <div className="modal-action">
+                            <button className="btn" type="submit">Rename</button>
+                            <button className="btn" onClick={handleClose}>Close</button>
+                        </div>
+                    </form>
+                </div>
+            </dialog>
+        </>
+    )
+}
