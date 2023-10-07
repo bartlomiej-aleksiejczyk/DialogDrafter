925b31d353849218272c606de00eaa081c9f3da2
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-07 03:31:21 +0200

    Added reactive modals to Main Component Container

--- src/components/mainComponentContainer/MainComponentContainer.tsx
+++ src/components/mainComponentContainer/MainComponentContainer.tsx
@@ -1,6 +1,7 @@
 import {ChatLogBoxContainer} from "../chatLogBox/ChatLogBoxContainer";
 import SideMenu from "../sideMenu/SideMenu";
-import {TopNavbar} from "../topNavbar/TopNavbar";import {useContext, useEffect} from "react";
+import {TopNavbar} from "../topNavbar/TopNavbar";
+import {useContext, useEffect, useRef, useState} from "react";
 import {NewFileModal} from "../newFileModal/NewFileModal";
 import {AddDirectoryModal} from "../addDirectoryModal/AddDirectoryModal";
 import {defaultConfig} from "../../shared/config/defaultConfig";
@@ -8,6 +9,7 @@ import {defaultConfig} from "../../shared/config/defaultConfig";
 import {ApplicationConfigContext} from "../initialConfig/ApplicationConfigContext";
 import {toast} from "react-hot-toast";
 import {SuccessToast} from "../../shared/toasts/SuccessToast";
+import _ from "lodash";
 
 export function MainComponentContainer() {
     //TODO: Add handling for specific changes in config. For example "Added new directory" instead of "Config successfully changed"
@@ -22,28 +24,40 @@ export function MainComponentContainer() {
     // TODO: Check if working file is in working directory
     // TODO: Error handling for I/O errors
     // TODO: Examine accessing object key as string e.g., "object["key"]"
-
+    const [isNewFileModalVisible, setIsNewFileModalVisible] = useState<boolean>(false);
+    const [isAddDirectoryModalVisible, setIsAddDirectoryModalVisible] = useState<boolean>(false);
     const {
         applicationConfig,
     } = useContext(ApplicationConfigContext)
+    const prevConfig = useRef(applicationConfig);
+
     useEffect(() => {
+        console.log(applicationConfig)
         window.ipcRenderer.send("saveConfigFile", applicationConfig, defaultConfig.SETTINGS_PATH);
-        window.ipcRenderer.on('saveConfigFileSuccess', (event, data) => {
+        window.ipcRenderer.once('saveConfigFileSuccess', (event, data) => {
             console.log(data.message);
-            toast.custom(SuccessToast(data.message));
+            const {directories: oldDirectories, workingFile: oldWorkingFile, ...oldConfig} = prevConfig.current;
+            const {directories: newDirectories, workingFile: newWorkingFile, ...newConfig} = applicationConfig;
+
+            if ((!_.isEqual(oldConfig, newConfig)) || (oldDirectories !== newDirectories)) {
+                toast.custom(SuccessToast(data.message));
+            }
+            prevConfig.current = applicationConfig;
         });
         return () => {
         };
     }, [applicationConfig]);
     return (
-            <div className="flex flex-col">
-                <TopNavbar />
-                <div className="flex flex-row">
-                    <SideMenu />
-                    <ChatLogBoxContainer/>
-                </div>
-                <NewFileModal/>
-                <AddDirectoryModal/>
+        <div className="flex flex-col">
+            <TopNavbar setIsAddDirectoryModalVisible={setIsAddDirectoryModalVisible}
+                       setIsNewFileModalVisible={setIsNewFileModalVisible}/>
+            <div className="flex flex-row">
+                <SideMenu/>
+                <ChatLogBoxContainer/>
             </div>
+            <NewFileModal isModalVisible={isNewFileModalVisible} setIsModalVisible={setIsNewFileModalVisible}/>
+            <AddDirectoryModal isModalVisible={isAddDirectoryModalVisible}
+                               setIsModalVisible={setIsAddDirectoryModalVisible}/>
+        </div>
     )
 }
