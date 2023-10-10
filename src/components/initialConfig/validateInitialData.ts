0fd7fdb52b39f7d9c99c5fe300a2039bfe76641d
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-10 17:04:53 +0200

    Extract validate config to shared

--- src/components/initialConfig/validateInitialData.ts
+++ src/components/initialConfig/validateInitialData.ts
@@ -1,38 +1,18 @@
-import Ajv from "ajv"
-import {defaultConfig} from "../../shared/config/defaultConfig";
 import _ from "lodash";
 import {toast} from "react-hot-toast";
 import {ErrorToast} from "../../shared/toasts/ErrorToast";
+import {validateConfig, validateConfigProps} from "../../shared/validators/validateConfig";
 
 // TODO: Add migration mechanism
-export function validateInitialData (initialData, setInitialData) {
+interface validateInitialDataProps extends validateConfigProps {
+    setInitialData: (data: Record<string, unknown>) => void;
+}
 
-    const validatedData = _.cloneDeep(initialData);
-    const ajv = new Ajv({useDefaults: true})
+export function validateInitialData({data, setInitialData}: validateInitialDataProps) {
 
-    const schema = {
-        "type": "object",
-        "properties": {
-            "workingFile": {"type": "string", "default": ""},
-            "directories": {
-                "type": "object",
-                "default": {},
-                "patternProperties": {
-                    "^[\\w-]+$": {"type": "string"}
-                },
-                "additionalProperties": false
-            },
-            "version": {"type": "number", "default": defaultConfig.CURRENT_VERSION}
-        },
-        "required": ["workingFile", "directories", "version"],
-        "additionalProperties": true
-    };
-    const validate = ajv.compile(schema)
-    validate(validatedData)
-
-    if (!_.isEqual(initialData, validatedData)) {
-        console.error("Validation Errors:");
+    const {newData, oldData} = validateConfig({data})
+    if (!_.isEqual(oldData, newData)) {
         toast.custom(ErrorToast("Your configuration file is has missing or invalid properties that were set to default values"));
-        setInitialData(validatedData)
+        setInitialData(newData)
     }
 }
