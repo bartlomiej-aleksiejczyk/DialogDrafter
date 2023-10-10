3c136bf07398defd9297e21cdd137a3abcf91f3c
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-10 17:13:20 +0200

    Remove any type from setApplicationConfig

--- src/components/initialConfig/SetInitialData.tsx
+++ src/components/initialConfig/SetInitialData.tsx
@@ -5,7 +5,7 @@ import {validateInitialData} from "./validateInitialData";
 
 interface SetInitialDataProps {
     applicationConfig: Record<string, string>;
-    setApplicationConfig: Dispatch<SetStateAction<Record<string, string>>>;
+    setApplicationConfig: Dispatch<SetStateAction<Record<string, unknown>>>;
     workingDirectory: string;
     setWorkingDirectory: Dispatch<SetStateAction<string>>;
     userPlatform: string;
@@ -18,7 +18,9 @@ export function SetInitialData({
         userPlatform}: SetInitialDataProps) {
     // TODO: use "electron-store" and "Ajv JSON schema validator" instead of custom solution, if something is not right display info about broken config file
     // TODO: add option to restore/add default config in case of broken file
-    validateInitialData(applicationConfig, setApplicationConfig)
+
+    // TODO: FIX type error
+    validateInitialData({applicationConfig, setApplicationConfig})
     return (
         <ApplicationConfigContext.Provider
             value={{
