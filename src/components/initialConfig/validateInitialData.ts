d9f05f765b843d063207fedc8b67bdfb1a94f2ce
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-12 23:28:17 +0200

    Add validators from initial data and config

--- src/components/initialConfig/validateInitialData.ts
+++ src/components/initialConfig/validateInitialData.ts
@@ -1,18 +1,21 @@
 import _ from "lodash";
-import {toast} from "react-hot-toast";
-import {ErrorToast} from "../../shared/toasts/ErrorToast";
-import {validateConfig, validateConfigProps} from "../../shared/validators/validateConfig";
+import { toast } from "react-hot-toast";
+import { ErrorToast } from "../../shared/toasts/ErrorToast";
+import { validateConfig, validateConfigProps } from "../../shared/validators/validateConfig";
 
 // TODO: Add migration mechanism
 interface validateInitialDataProps extends validateConfigProps {
-    setInitialData: (data: Record<string, unknown>) => void;
+	setInitialData: (data: Record<string, unknown>) => void;
 }
 
-export function validateInitialData({data, setInitialData}: validateInitialDataProps) {
-
-    const {newData, oldData} = validateConfig({data})
-    if (!_.isEqual(oldData, newData)) {
-        toast.custom(ErrorToast("Your configuration file is has missing or invalid properties that were set to default values"));
-        setInitialData(newData)
-    }
+export function validateInitialData({ data, setInitialData }: validateInitialDataProps) {
+	const { newData, oldData } = validateConfig({ data: data });
+	if (!_.isEqual(oldData, newData)) {
+		toast.custom(
+			ErrorToast(
+				"Your configuration file is has missing or invalid properties that were set to default values",
+			),
+		);
+		setInitialData(newData);
+	}
 }
--- src/shared/validators/validateConfig.ts
+++ src/shared/validators/validateConfig.ts
@@ -1,32 +1,32 @@
-import {defaultConfig} from "../config/defaultConfig";
-import Ajv from "ajv"
+import { defaultConfig } from "../config/defaultConfig";
+import Ajv from "ajv";
 import _ from "lodash";
 
 export interface validateConfigProps {
-    data: Record<string, unknown>
+	data: Record<string, unknown>;
 }
 
-export function validateConfig({data}: validateConfigProps) {
-    const validatedData: Record<string, unknown> = _.cloneDeep(data);
-    const ajv = new Ajv({useDefaults: true})
-    const schema = {
-        "type": "object",
-        "properties": {
-            "workingFile": {"type": "string", "default": ""},
-            "directories": {
-                "type": "object",
-                "default": {},
-                "patternProperties": {
-                    "^[\\w-]+$": {"type": "string"}
-                },
-                "additionalProperties": false
-            },
-            "version": {"type": "number", "default": defaultConfig.CURRENT_VERSION}
-        },
-        "required": ["workingFile", "directories", "version"],
-        "additionalProperties": true
-    };
-    const validate = ajv.compile(schema)
-    validate(validatedData)
-    return {newData:validatedData, oldData: data}
+export function validateConfig({ data }: validateConfigProps) {
+	const validatedData: Record<string, unknown> = _.cloneDeep(data);
+	const ajv = new Ajv({ useDefaults: true });
+	const schema = {
+		type: "object",
+		properties: {
+			workingFile: { type: "string", default: "" },
+			directories: {
+				type: "object",
+				default: {},
+				patternProperties: {
+					"^[\\w-]+$": { type: "string" },
+				},
+				additionalProperties: false,
+			},
+			version: { type: "number", default: defaultConfig.CURRENT_VERSION },
+		},
+		required: ["workingFile", "directories", "version"],
+		additionalProperties: true,
+	};
+	const validate = ajv.compile(schema);
+	validate(validatedData);
+	return { newData: validatedData, oldData: data };
 }
