ed6d7e993f94ba5edf9613a16928000efcbcff35
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-12 17:05:12 +0200

    Format useFileManagment

--- src/components/newFileModal/useFileManagment.ts
+++ src/components/newFileModal/useFileManagment.ts
@@ -2,18 +2,18 @@ import { toast } from "react-hot-toast";
 import { SuccessToast } from "../../shared/toasts/SuccessToast";
 
 export const useFileManagement = (applicationConfig, setApplicationConfig) => {
-    const handleNewFileData = (_event, data) => {
-        console.log(data);
-        setApplicationConfig({
-            ...applicationConfig,
-            "working-file": data.newFilePath
-        });
-        toast.custom(SuccessToast(data.message));
-    };
+	const handleNewFileData = (_event, data) => {
+		console.log(data);
+		setApplicationConfig({
+			...applicationConfig,
+			workingFile: data.newFilePath,
+		});
+		toast.custom(SuccessToast(data.message));
+	};
 
-    const createNewFile = (filePath) => {
-        window.ipcRenderer.send("save-working-file", "", filePath);
-        window.ipcRenderer.once("save-working-file-success", handleNewFileData);
-    };
-    return { createNewFile };
+	const createNewFile = (filePath) => {
+		window.ipcRenderer.send("save-working-file", "", filePath);
+		window.ipcRenderer.once("save-working-file-success", handleNewFileData);
+	};
+	return { createNewFile };
 };
