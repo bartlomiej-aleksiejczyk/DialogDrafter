38702ee33c2a738abff703c6a8138885a8dedc25
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-13 16:25:41 +0200

    Extract message to external file

--- src/components/initialConfig/validateInitialData.ts
+++ src/components/initialConfig/validateInitialData.ts
@@ -2,6 +2,7 @@ import _ from "lodash";
 import { toast } from "react-hot-toast";
 import { ErrorToast } from "../../shared/toasts/ErrorToast";
 import { validateConfig, validateConfigProps } from "../../shared/validators/validateConfig";
+import { CONFIG_PARSING_ERROR_MESSAGE } from "./errorMessages";
 
 // TODO: Add migration mechanism
 interface validateInitialDataProps extends validateConfigProps {
@@ -11,11 +12,7 @@ interface validateInitialDataProps extends validateConfigProps {
 export function validateInitialData({ data, setInitialData }: validateInitialDataProps) {
 	const { newData, oldData } = validateConfig({ data: data });
 	if (!_.isEqual(oldData, newData)) {
-		toast.custom(
-			ErrorToast(
-				"Your configuration file is has missing or invalid properties that were set to default values",
-			),
-		);
+		toast.custom(ErrorToast(CONFIG_PARSING_ERROR_MESSAGE));
 		setInitialData(newData);
 	}
 }
