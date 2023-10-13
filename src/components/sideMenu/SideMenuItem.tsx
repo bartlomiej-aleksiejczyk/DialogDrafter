73da5eee0761d74ced8bc9af2f82e664a5bec629
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-13 16:48:55 +0200

    Reformat SideMenuItem using prettier

--- src/components/sideMenu/SideMenuItem.tsx
+++ src/components/sideMenu/SideMenuItem.tsx
@@ -1,32 +1,38 @@
-import {useContext} from "react";
-import {ApplicationConfigContext} from "../initialConfig/ApplicationConfigContext";
-import {joinPath} from "../../shared/utils/joinPath";
+import { useContext } from "react";
+import { ApplicationConfigContext } from "../initialConfig/ApplicationConfigContext";
+import { joinPath } from "../../shared/utils/path/joinPath";
 
 interface SideMenuItemProps {
-    fileName: string
-    directory: string
+	fileName: string;
+	directory: string;
 }
-export function SideMenuItem ({fileName, directory}: SideMenuItemProps) {
-    const {applicationConfig, setApplicationConfig, workingDirectory, userPlatform} = useContext(ApplicationConfigContext)
-    const newWorkingFilePath = joinPath([directory, fileName], userPlatform);
 
+export function SideMenuItem({ fileName, directory }: SideMenuItemProps) {
+	const { applicationConfig, setApplicationConfig, workingDirectory, userPlatform } =
+		useContext(ApplicationConfigContext);
+	const newWorkingFilePath = joinPath([directory, fileName], userPlatform);
 
-    const handleSelectFile = () => {
-        setApplicationConfig({
-            ...applicationConfig,
-            "workingFile": newWorkingFilePath
-        })
-    };
+	const handleSelectFile = () => {
+		setApplicationConfig({
+			...applicationConfig,
+			workingFile: newWorkingFilePath,
+		});
+	};
 
-    return (
-        <li className="pt-2">
-            <a onClick={() => handleSelectFile()}
-               className={`${
-               (newWorkingFilePath === applicationConfig["workingFile"] &&
-                   directory === workingDirectory) ? "active" : ""} truncate w-52 block 
-                           scroll-on-hover`}>
-                {fileName}
-            </a>
-        </li>
-    );
+	return (
+		<li className="pt-2">
+			<a
+				onClick={() => handleSelectFile()}
+				className={`${
+					newWorkingFilePath === applicationConfig["workingFile"] &&
+					directory === workingDirectory
+						? "active"
+						: ""
+				} scroll-on-hover block w-52 
+                           truncate`}
+			>
+				{fileName}
+			</a>
+		</li>
+	);
 }
