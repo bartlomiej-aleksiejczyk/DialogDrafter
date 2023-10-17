4cbcf01b72b90047121941539ca23e1364335b93
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-17 22:17:06 +0200

    Add component to display parsed markdown

---
+++ src/components/addDirectoryModal/MarkdownPreview.tsx
@@ -0,0 +1,19 @@
+import hljs from "highlight.js";
+import { useEffect } from "react";
+import "highlight.js/styles/base16/bright.css";
+
+interface MarkdownPreviewProps {
+	parsedMarkdown: string;
+}
+
+export function MarkdownPreview({ parsedMarkdown }: MarkdownPreviewProps) {
+	useEffect(() => {
+		hljs.highlightAll();
+	}, []);
+	return (
+		<>
+			<hr className="rounded-xl border-t-8 border-solid"/>
+			<div className="markdown-content" dangerouslySetInnerHTML={{ __html: parsedMarkdown }} />
+		</>
+	);
+}
